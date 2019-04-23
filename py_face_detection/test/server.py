1ccec4d82c6591efb7482799db196c49b9952299
Trijeet Modak <uniquetrij@gmail.com>
2019-04-23 20:50:52 +0530

    flask movie integration

--- py_face_detection/test/server.py
+++ py_face_detection/test/server.py
@@ -1,5 +1,5 @@
 from flask import Flask, json, Response
-
+from flask_cors import CORS
 import cv2
 from py_pipe.pipe import Pipe
 
@@ -18,7 +18,6 @@ from py_tensorflow_runner.session_utils import SessionRunner
 from py_face_detection.comparator_api.embedding_generator import EmbeddingGenerator
 from proj_data.py_face_detection.embeddings.embeddings import emp
 
-
 session_runner = SessionRunner()
 
 generator = EmbeddingGenerator()
@@ -27,13 +26,13 @@ generator_op = generator.get_out_pipe()
 generator.use_session_runner(session_runner)
 generator.run()
 
-
-pipe = Pipe()
+pipe = Pipe(limit=1)
 fs = FlaskMovie(app=app)
-fs.create('face_image', pipe)
+fs.create('face_image', pipe, np.zeros((160, 160, 3)), timeout=1)
+
 
 @app.route('/<emp_id>/<direction>')
-def processReq(emp_id,direction):
+def processReq(emp_id, direction):
     cap = cv2.VideoCapture(-1)
     ret, image = cap.read()
     if not ret:
@@ -49,11 +48,12 @@ def processReq(emp_id,direction):
         face_image = inference.get_meta('face_image')
         pipe.push(face_image)
         d = np.sqrt(np.sum(np.square(np.subtract(embedding, emp[emp_id]))))
-        print(emp_id,d)
+        print(emp_id, d)
         if d < 0.85:
             requests.get("http://192.168.254.1:5000/turnstile", {'direction': direction})
-
-
+            return Response(json.dumps("{'result': valid }"), status=200, mimetype='application/json')
+        return Response(json.dumps("{'result': invalid }"), status=200, mimetype='application/json')
+    return Response(json.dumps("{'result': unknown }"), status=200, mimetype='application/json')
 
 
 
